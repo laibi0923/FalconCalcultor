854b1b5e2e2da2314b41fb95ddc19242046b7ef8
laibi0923 <laibi0923@gmail.com>
2017-12-12 01:19:35 +0800

    update

---
+++ app/src/main/java/com/savtor/falconcalcultorSchedule/Schedule_Data.java
@@ -0,0 +1,18 @@
+package com.savtor.falconcalcultorSchedule;
+
+/**
+ * Created by GhostLeo_DT on 26/11/2017.
+ */
+public class Schedule_Data {
+
+    public String sc_data_trems, sc_data_insterest, sc_data_principle, sc_data_balance;
+
+    public Schedule_Data(String scv_data_trems, String scv_data_insterest, String scv_data_principle, String scv_data_balance){
+
+        this.sc_data_trems = scv_data_trems;
+        this.sc_data_insterest = scv_data_insterest;
+        this.sc_data_principle = scv_data_principle;
+        this.sc_data_balance = scv_data_balance;
+    }
+
+}
