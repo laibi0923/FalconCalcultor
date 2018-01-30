b678d5e80b9300bfe94299d4804d2d1e78f65259
laibi0923 <laibi0923@gmail.com>
2018-01-31 00:51:38 +0800

    update 31/1/2018

--- app/src/main/java/com/savtor/Debit_Info/DebitInfo_Main.java
+++ app/src/main/java/com/savtor/Debit_Info/DebitInfo_Main.java
@@ -11,17 +11,12 @@ import android.widget.*;
 public class DebitInfo_Main extends Fragment
 {
 
-	private View Page_Personal, Page_Mortgage;
-	private Typer_TextView Personal_TextView, Mort_TextView;
-	private ImageView Personal_Icon;
+	private View Page_Personal, Page_Mortgage, Page_Revolving, Page_Car, Page_Card;
+	private TextView Personal_Title, Mortgage_Title, Revolving_Title, Car_Title, Card_Title;
+	private ImageView Personal_Icon, Mortgage_Icon, Revolving_Icon, Car_Icon, Card_Icon;
 	private List<View> Page_List;
 	private ViewPager mViewPager;
 	private DebitInfo_Adapter mAdapter;
-	private String Total_Count_Text = "已批出貸款共 ";
-	private String Total_Amount = "總貸款銀碼$ ";
-	private String Toal_Insterest = "預計總利息支出$ ";
-	private String Month_Pay = "每月共支出$ ";
-	private String Month_Insterest = "每月利息支出$ ";
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
@@ -37,28 +32,50 @@ public class DebitInfo_Main extends Fragment
 	private void Find_View(View v, LayoutInflater inflater){
 		
 		Page_Personal = inflater.inflate(R.layout.zz_tseting_debit_viewpapger, null);
+		Personal_Title = (TextView) Page_Personal.findViewById(R.id.product_type);
+		Personal_Title.setText(getString(R.string.title_product_personal));
 		Personal_Icon = (ImageView) Page_Personal.findViewById(R.id.product_type_icon);
 		Personal_Icon.setImageResource(R.drawable.ic_person_black_48dp);
-		Personal_TextView = (Typer_TextView) Page_Personal.findViewById(R.id.typer_textview);
-		Personal_TextView.setPrintText(Total_Count_Text + "個\n" +
-		Total_Amount + "18,000\n" +
-		Toal_Insterest + "5,000\n" +
-		Month_Pay + "60,000\n" +
-		Month_Insterest + "30,000"
-		, 50, "_");
-		Personal_TextView.startPrint();
+
 		
 		Page_Mortgage = inflater.inflate(R.layout.zz_tseting_debit_viewpapger, null);
-		Mort_TextView = (Typer_TextView) Page_Mortgage.findViewById(R.id.typer_textview);
-		Mort_TextView.setPrintText("12445678890065433678", 100, "|");
-		Mort_TextView.startPrint();
-		
+		Mortgage_Title = (TextView) Page_Mortgage.findViewById(R.id.product_type);
+		Mortgage_Title.setText(getString(R.string.title_product_mortgage));
+		Mortgage_Icon = (ImageView) Page_Mortgage.findViewById(R.id.product_type_icon);
+		Mortgage_Icon.setImageResource(R.drawable.ic_location_city_black_48dp);
+
+
+		Page_Revolving = inflater.inflate(R.layout.zz_tseting_debit_viewpapger, null);
+		Revolving_Title = (TextView) Page_Revolving.findViewById(R.id.product_type);
+		Revolving_Title.setText(getString(R.string.title_product_revolving));
+		Revolving_Icon = (ImageView) Page_Revolving.findViewById(R.id.product_type_icon);
+		Revolving_Icon.setImageResource(R.drawable.ic_loop_black_48dp);
+
+
+		Page_Car = inflater.inflate(R.layout.zz_tseting_debit_viewpapger, null);
+		Car_Title = (TextView) Page_Car.findViewById(R.id.product_type);
+		Car_Title.setText(getString(R.string.title_product_car));
+		Car_Icon = (ImageView) Page_Car.findViewById(R.id.product_type_icon);
+		Car_Icon.setImageResource(R.drawable.ic_airport_shuttle_black_48dp);
+
+
+		Page_Card = inflater.inflate(R.layout.zz_tseting_debit_viewpapger, null);
+		Card_Title = (TextView) Page_Card.findViewById(R.id.product_type);
+		Card_Title.setText(getString(R.string.title_product_card));
+		Card_Icon = (ImageView) Page_Card.findViewById(R.id.product_type_icon);
+		Card_Icon.setImageResource(R.drawable.ic_credit_card_black_48dp);
+
+
 		Page_List = new ArrayList<View>();
 		Page_List.add(Page_Personal);
 		Page_List.add(Page_Mortgage);
+		Page_List.add(Page_Revolving);
+		Page_List.add(Page_Car);
+		Page_List.add(Page_Card);
+
 		
 		
-		mViewPager = (ViewPager) v.findViewById(R.id.mviewPager);
+		mViewPager = (ViewPager) v.findViewById(R.id.debit_viewpager);
 		mAdapter = new DebitInfo_Adapter(Page_List);
 		mViewPager.setAdapter(mAdapter);
 	}
